926baecbf185b98f2d7e288f36cd47d784223ea2
Sam Yeager <sam.williams@littlebytegames.com>
2020-06-19 17:38:17 -0700

    add Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <section id="footer">
+    This is the footer
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+#footer {
+  width: 100%;
+  height: 50px;
+  background-color: rgba(0, 0, 0, 0.25);
+}
+</style>
